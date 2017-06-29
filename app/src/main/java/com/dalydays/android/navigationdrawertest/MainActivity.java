da6065f8810e54be6c01136c6747a805039abd6d
linucksrox <ericdalyblog@dalydays.com>
2017-06-29 19:48:40 -0400

    Create custom action bar, overlay navdrawer on top I don't like this style, but it's interested to have tried it out

--- app/src/main/java/com/dalydays/android/navigationdrawertest/MainActivity.java
+++ app/src/main/java/com/dalydays/android/navigationdrawertest/MainActivity.java
@@ -4,17 +4,22 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.nav_drawer_open, R.string.nav_drawer_close);
